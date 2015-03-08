5a4fa9afd835545eb68c9e21279c0a1ef0b8808c
Rommel Rico <rommeltj@gmail.com>
2015-03-07 23:11:38 -0800

    Fixing crash when tapping items

--- BlogReader/MasterViewController.swift
+++ BlogReader/MasterViewController.swift
@@ -63,9 +63,7 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow() {
-            let object = self.fetchedResultsController.objectAtIndexPath(indexPath) as NSManagedObject
                 let controller = (segue.destinationViewController as UINavigationController).topViewController as DetailViewController
-                controller.detailItem = object
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
                 controller.navigationItem.leftItemsSupplementBackButton = true
             }
